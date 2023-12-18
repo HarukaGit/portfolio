9cdfc88760c743bece2e1cf6a699fe6100e77d76
haruka <nvdw23065@nvrcd.ac.jp>
2023-12-18 16:52:53 +0900

    cssレスポンシブ

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 'use strict'
 
+if (window.matchMedia('(max-width: 600px)').matches) {
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  let addclass = document.getElementById('works').classList.add('container-width')
+  console.log(addclass)
+}
+
+
 
 gsap.to("h1", {
     duration: 2,
